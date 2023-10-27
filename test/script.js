11c34f33bd65ac6f6d8928ddd98d1d2e597f7889
Eusebio Resende <ere3@3ds.com>
2023-10-27 11:00:12 -0400

    Add tests

--- test/script.js
+++
@@ -1,4 +0,0 @@
-const sayHello = require('say-hello')
-
-console.log(sayHello())
-
---
+++ test/script.test.js
@@ -0,0 +1,6 @@
+const sayHello = require('say-hello')
+
+test('Just say Hello!', () => {
+    expect(sayHello()).toBe("Hello!!!")
+});
+
